012fadff061403e05a8edbce578c79f81b91fda9
Jared Young <79735672+jyoung10078@users.noreply.github.com>
2025-03-20 22:17:39 -0500

    Setting up app routes for first games

---
+++ pages/tensies/page.tsx
@@ -0,0 +1,14 @@
+
+import Link from 'next/link';
+
+export default function Page() {
+    return(
+        <>
+            <h1>Welcome to the tensies page!</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2> 
+        </>
+
+    )
+}
