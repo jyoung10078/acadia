07ac33db7fef270ee97fa6a6ffb0208df67e6707
Jared Young <jyoung10078@gmail.com>
2025-03-31 11:50:33 -0500

    Initial Layout of Tensies

--- pages/tensies/page.tsx
+++ pages/tensies/page.tsx
@@ -1,14 +1,28 @@
-
+'use client';
 import Link from 'next/link';
-import styles from '../tensies/tensies.modules.css';
+import styles from '../../styles/Home.module.css';
+import { useState } from 'react';
 
 export default function Page() {
+    const [diceArray, setDiceArray] = useState<number[]>([1,3,4,4,2,3,5,6,2,1]);
+
+
+
     return(
         <>
-            <h1>Welcome to the tensies page!</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2> 
+            <body>
+                <div className={styles.specialcard}>
+                    <h1 className={styles.title}>Tenzies</h1>
+                    <p className={styles.description}>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+                    <div className={styles.dice_container}>
+                        {diceArray.map((die) => <button className={styles.die}>{die}</button>)}
+                    </div>
+                    <h2>
+                        <Link href="/">Back to home</Link>
+                    </h2>
+                </div>
+            </body>
+            
         </>
 
     )
